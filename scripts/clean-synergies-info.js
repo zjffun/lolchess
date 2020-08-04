7f86dd7c1e8c5d1227c81a34c30796848c31dd88
JuFeng Zhang <zjffun@gmail.com>
2020-08-04 20:14:54 +0800

    add scripts about synergies

---
+++ scripts/clean-synergies-info.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+const path = require("path");
+const { JSDOM } = require("jsdom");
+const herosDetails = require("../src/assets/heros/details.en.json");
+
+function getKeyword(name) {
+  return name.toLowerCase().replace(/ /g, "-");
+}
+
+function getInfo(type) {
+  return function (dom) {
+    // header
+    const header = dom.querySelector(".guide-synergy-table__synergy__header");
+    const imgURL = header.querySelector("img").src;
+    const name = header.querySelector(".align-middle").textContent.trim();
+    const keyword = getKeyword(name);
+
+    // content
+    const content = dom.querySelector(".guide-synergy-table__synergy__content");
+    const heros = Array.from(
+      content
+        .querySelector(".guide-synergy-table__synergy__champions")
+        .querySelectorAll("img")
+    ).map((d) => {
+      return herosDetails.find((heroDetail) => heroDetail.name === d.alt)
+        .keyword;
+    });
+    const descDom = content.querySelector(
+      ".guide-synergy-table__synergy__desc"
+    );
+    const desc = descDom ? descDom.textContent.trim() : "";
+    const stats = Array.from(
+      content
+        .querySelector(".guide-synergy-table__synergy__stats")
+        .querySelectorAll("div")
+    ).map((d) => d.textContent.trim());
+
+    return {
+      imgURL,
+      name,
+      keyword,
+      heros,
+      desc,
+      stats,
+      type,
+    };
+  };
+}
+async function main() {
+  const baseDir = path.resolve(__dirname, "../src/assets/synergies");
+
+  const dom = JSDOM.fragment(
+    String(fs.readFileSync(path.resolve(__dirname, "./data/synergies.info")))
+  );
+  const synergies = dom.querySelector(".guide-synergy-table");
+  const cols = synergies.querySelectorAll(".col-lg-6");
+
+  const result = [].concat(
+    // Origins
+    Array.from(cols[0].querySelectorAll(".guide-synergy-table__synergy")).map(
+      getInfo("origins")
+    ), // Classes
+    Array.from(cols[1].querySelectorAll(".guide-synergy-table__synergy")).map(
+      getInfo("classes")
+    )
+  );
+
+  fs.writeFileSync(
+    path.resolve(__dirname, "./data/synergies-img-info.json"),
+    JSON.stringify(
+      result.map((d) => {
+        return {
+          imgURL: `https:${d.imgURL}`,
+          keyword: d.keyword,
+        };
+      }),
+      null,
+      2
+    )
+  );
+
+  fs.writeFileSync(
+    path.resolve(baseDir, "infos.en.json"),
+    JSON.stringify(
+      result.map((d) => {
+        return {
+          ...d,
+          imgURL: undefined,
+        };
+      }),
+      null,
+      2
+    )
+  );
+}
+
+main();
