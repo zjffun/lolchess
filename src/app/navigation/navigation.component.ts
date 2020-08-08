fa27bebf007315d86668b073af19cc59d8dd1632
JuFeng Zhang <zjffun@gmail.com>
2020-08-08 17:41:12 +0800

    fix sidenav click bug

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navigation',
@@ -9,6 +10,9 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
+  @ViewChild('drawer') drawer: MatSidenav;
+
+  isHandset;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -16,5 +20,20 @@ export class NavigationComponent {
       shareReplay()
     );
 
+  handleLinkClick() {
+    if (this.isHandset) {
+      this.drawer.close();
+    }
+  }
+
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.isHandset$.subscribe((val) => {
+      console.log(val);
+      this.isHandset = val;
+    });
+  }
+
+  ngOnDestroy() {}
 }
